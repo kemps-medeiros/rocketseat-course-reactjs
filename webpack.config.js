69165d888954089f19cf396180aded0e21cfedbc
Kemps Medeiros <kempsmm@gmail.com>
2022-05-30 21:37:46 -0300

    added component RepositoryList and sass configuration

--- webpack.config.js
+++ webpack.config.js
@@ -24,9 +24,16 @@ module.exports = {
     ],
     module: {
         rules: [{
-            test: /\.jsx$/,
-            exclude: /node_modules/,
-            use: 'babel-loader',
-        }],
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+
+        ],
     }
 };
